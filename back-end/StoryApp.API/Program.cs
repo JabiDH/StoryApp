706fb8e7ef1703a58f298857c9faabfa67d83da5
Jabi Habash <Jabi.Habash@landisgyr.com>
2025-01-14 20:35:05 -0500

    Adds startup class
    Identification: 1
    Change-Id: I91bd1425be8dcb0429b2db5df6979eb1ad6cc2cb

--- back-end/StoryApp.API/Program.cs
+++ back-end/StoryApp.API/Program.cs
@@ -1,56 +1,16 @@
 using Serilog;
-using StoryApp.API.Middlewares;
-using StoryApp.API.Services;
+using StoryApp.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Infinite)
-    .Enrich.FromLogContext()
-    .ReadFrom.Configuration(builder.Configuration)
-    .CreateLogger();
+// Add Startup
+var startup = new Startup(builder.Configuration);
+startup.ConfigureLogging(builder.Host);
 
-builder.Host.UseSerilog();
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddHttpClient();
-
-builder.Services.AddMemoryCache();
-
-builder.Services.AddScoped<ICacheBackService, CacheBackService>();
-
-builder.Services.AddScoped<IStoriesService, StoriesService>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-});
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.UseMiddleware<ExceptionMiddleware>();
-
-app.UseCors();
-
-app.MapControllers();
+startup.Configure(app, app.Environment);
 
 app.Run();
---
+++ back-end/StoryApp.API/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Serilog;
+using Microsoft.OpenApi.Models;
+using StoryApp.API.Middlewares;
+using StoryApp.API.Services;
+
+namespace StoryApp.API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureLogging(IHostBuilder hostBuilder)
+        {
+            // Set up Serilog
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Infinite)
+                .Enrich.FromLogContext()
+                .ReadFrom.Configuration(_configuration)
+                .CreateLogger();
+
+            hostBuilder.UseSerilog();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "StoryApp.API", Version = "v1" });
+            });
+
+            services.AddHttpClient();
+            services.AddMemoryCache();
+
+            services.AddScoped<ICacheBackService, CacheBackService>();
+            services.AddScoped<IStoriesService, StoriesService>();
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment() || env.IsProduction())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseCors();
+
+            app.MapControllers();
+        }
+    }
+}
